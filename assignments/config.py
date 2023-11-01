d0fba0ab1f50d0832a4c5ea617cd0bb568a05e5c
emma2d <emma2d@gmail>
2023-11-01 13:48:56 +0000

    Task 4 complete

---
+++ assignments/config.py
@@ -0,0 +1,7 @@
+configkey = {
+"htmltopdfkey":"W6LPG8DPSp46mA9qp5RZQd1N8wBCK06DlRQuu8Hnc9Wa4BSN56rmz7z2j6XkqmTb"
+}
+
+configkeyprivate = {
+    "privatekey":"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"
+}
