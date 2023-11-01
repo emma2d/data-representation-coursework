c22090d0768ad5e83d086fb58e43ef17d94a9a6a
emma2d <124067038+emma2d@users.noreply.github.com>
2023-11-01 13:54:17 +0000

    Delete assignments/config.py
    Added in error

--- assignments/config.py
+++
@@ -1,7 +0,0 @@
-configkey = {
-"htmltopdfkey":"W6LPG8DPSp46mA9qp5RZQd1N8wBCK06DlRQuu8Hnc9Wa4BSN56rmz7z2j6XkqmTb"
-}
-
-configkeyprivate = {
-    "privatekey":"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"
-}
